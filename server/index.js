d3a62766a2dab373ee2431e9135942779c4cb18e
vinay <vinayydv2003@gmail.com>
2026-02-23 12:05:50 +0530

    Update CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const port=process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-     origin:"http://localhost:5173",
+     origin:"https://genwebai-1-bmhd.onrender.com",
      credentials:true 
 }))
 app.use("/api/auth",authRouter)
