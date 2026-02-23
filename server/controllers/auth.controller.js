f717416069f8353f196ad96adbf7cd35b627f9fb
vinay <vinayydv2003@gmail.com>
2026-02-23 11:46:51 +0530

    Update cookie settings for security compliance

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -20,8 +20,8 @@ export const googleAuth=async(req,res)=>{
 
           res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"strict",
+            secure:true,
+            sameSite:"none",
             maxAge:7*24*60*60*1000
           })
           return res.status(200).json(user)
@@ -36,8 +36,8 @@ export const logOut=async(req,res)=>{
     try{
  res.clearCookie("token",{
     httpOnly:true,
-    secure:false,
-    sameSite:"Strict",
+    secure:true,
+    sameSite:"none",
    
   })
   return res.status(200).json({message:"logged out successfully"})
